b9951187ea8b206e788f2e7f0f150228bed23a62
Divine Dube <Divine.Dube@standardbank.co.za>
2019-09-10 23:32:12 +0200

    add internet perm

---
+++ app/src/androidTest/java/za/co/dubedivine/groceryapp/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package za.co.dubedivine.groceryapp
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("za.co.dubedivine.groceryapp", appContext.packageName)
+    }
+}
